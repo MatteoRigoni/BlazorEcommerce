d17ad0bb7cc008b5bb5efd369ff3a5223d7071c3
MatteoRigoni <matteo.rigoni2@gmail.com>
2022-04-14 23:32:34 +0200

    Add project files.

---
+++ BlazorEcommerce/Server/Data/DataContext.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace BlazorEcommerce.Server.Data
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options) : base(options)
+        {
+
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Product>().HasData(
+                new Product
+                {
+                    Id = 1,
+                    Title = "The Lord of the Rings",
+                    Description = "The title refers to the story's main antagonist, the Dark Lord Sauron",
+                    ImageUrl = "https://upload.wikimedia.org/wikipedia/commons/thumb/4/4d/TREEBEARD.jpg/225px-TREEBEARD.jpg",
+                    Price = 10.19m
+                },
+                 new Product
+                 {
+                     Id = 2,
+                     Title = "The Chronicles of Narnia",
+                     Description = " The series is set in the fictional realm of Narnia",
+                     ImageUrl = "https://upload.wikimedia.org/wikipedia/en/c/cb/The_Chronicles_of_Narnia_box_set_cover.jpg",
+                     Price = 20.99m
+                 });
+        }
+
+        public DbSet<Product> Products { get; set; }
+    }
+}
---
+++ BlazorEcommerce/Server/Migrations/20220414212929_ProductSeed.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace BlazorEcommerce.Server.Migrations
+{
+    public partial class ProductSeed : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "Products",
+                columns: new[] { "Id", "Description", "ImageUrl", "Price", "Title" },
+                values: new object[] { 1, "The title refers to the story's main antagonist, the Dark Lord Sauron", "https://upload.wikimedia.org/wikipedia/commons/thumb/4/4d/TREEBEARD.jpg/225px-TREEBEARD.jpg", 10.19m, "The Lord of the Rings" });
+
+            migrationBuilder.InsertData(
+                table: "Products",
+                columns: new[] { "Id", "Description", "ImageUrl", "Price", "Title" },
+                values: new object[] { 2, " The series is set in the fictional realm of Narnia", "https://upload.wikimedia.org/wikipedia/en/c/cb/The_Chronicles_of_Narnia_box_set_cover.jpg", 20.99m, "The Chronicles of Narnia" });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "Products",
+                keyColumn: "Id",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "Products",
+                keyColumn: "Id",
+                keyValue: 2);
+        }
+    }
+}
