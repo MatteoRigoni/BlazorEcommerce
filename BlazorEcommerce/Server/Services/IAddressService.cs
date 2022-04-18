869155c04b06c120f2dc418ea75f15d4b888bad1
MatteoRigoni <matteo.rigoni2@gmail.com>
2022-04-18 19:47:35 +0200

    Address info

---
+++ BlazorEcommerce/Server/Services/IAddressService.cs
@@ -0,0 +1,9 @@
+﻿
+namespace BlazorEcommerce.Server.Services
+{
+    public interface IAddressService
+    {
+        Task<ServiceResponse<Address>> AddOrUpdateAddress(Address address);
+        Task<ServiceResponse<Address>> GetAddress();
+    }
+}
